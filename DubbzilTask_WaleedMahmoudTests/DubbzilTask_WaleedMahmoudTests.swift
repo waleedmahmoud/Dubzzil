e9372e570b9787e8b45a38974a1870a1411bed65
waleed2020 <waleed@beuti.io>
2020-11-19 16:26:53 +0400

    Initial Commit

---
+++ DubbzilTask_WaleedMahmoudTests/DubbzilTask_WaleedMahmoudTests.swift
@@ -0,0 +1,36 @@
+//
+//  DubbzilTask_WaleedMahmoudTests.swift
+//  DubbzilTask_WaleedMahmoudTests
+//
+//  Created by Waleed on 11/19/20.
+//  Copyright © 2020 Waleed. All rights reserved.
+//
+
+import XCTest
+@testable import DubbzilTask_WaleedMahmoud
+
+class DubbzilTask_WaleedMahmoudTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
